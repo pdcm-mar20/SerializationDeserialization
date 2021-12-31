4a52d5cbed5ac4b9195b63917520474153c01c5e
pdcm-mar20 <63573499+pdcm-mar20@users.noreply.github.com>
2021-12-31 07:42:19 +0700

    latest

---
+++ SerializationDeserialization/UTest/UnitTest1.cs
@@ -0,0 +1,61 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerializationDeserializationLib;
+using System;
+using System.IO;
+
+namespace UTest
+{
+    public class UnitTest1
+    {
+        PlayerData dataTest = new PlayerData("Name Test", 10, 2000);
+        string path = Directory.GetCurrentDirectory() + "/PlayerDataTest.bin";
+
+        [Fact]
+        public void SerializeWorkTest()
+        {
+            Serializer serializer = new Serializer();
+
+            serializer.SerializeData(dataTest, path);
+        }
+
+        [Fact]
+        public void DeserializeWorkTest()
+        {
+            PlayerData data = new PlayerData();
+            Deserializer deserializer = new Deserializer();
+
+            data = deserializer.DeserializeData(path);
+        }
+
+        [Fact]
+        public void IsThereCreatedFileTest()
+        {
+            bool cond = true;
+
+            if (!File.Exists(path))
+            {
+                cond = false;
+            }
+
+            Assert.True(cond);
+        }
+
+        [Fact]
+        public void TestEqualData()
+        {
+            Serializer serializer = new Serializer();
+            serializer.SerializeData(dataTest, path);
+
+            PlayerData data = new PlayerData();
+            Deserializer deserializer = new Deserializer();
+            data = deserializer.DeserializeData(path);
+
+            bool cond = true;
+            if (data.playerName != dataTest.playerName) cond = false;
+            if (data.level != dataTest.level) cond = false;
+            if (data.gold != dataTest.gold) cond = false;
+
+            Assert.True(cond);
+        }
+    }
+}
